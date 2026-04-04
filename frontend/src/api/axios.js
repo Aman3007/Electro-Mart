66121878f730dc357dab1bbe7542f197beffe383
Aman <amanchauhan30072002@gmail.com>
2026-04-04 10:50:36 +0530

    Change API base URL to production endpoint

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://electro-mart-yuq0.onrender.com/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
