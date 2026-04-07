cdede9c5f88afbde42329b00d4aa43de80ec2ddc
Aman <amanchauhan30072002@gmail.com>
2026-04-07 19:31:27 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: 'https://electro-mart-yuq0.onrender.com',
+  baseURL: 'https://electro-mart-yuq0.onrender.com/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
